ee8bcf57866c5f161d9a462e0e1dfbd653f16b22
Hee Soo Song <108647584+star2871@users.noreply.github.com>
2023-01-11 15:18:09 +0900

    [Gold V] Title: 시간 관리하기, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/6068. 시간 관리하기/시간 관리하기.py
+++ 백준/Gold/6068. 시간 관리하기/시간 관리하기.py
@@ -1,13 +1,13 @@
-N = int(input())
-works = [list(map(int, input().split())) for _ in range(N)]
-works = sorted(works, key=lambda x: x[1])
-total_time = 0
-min_rest = float("inf")
-for time, limit in works:
-    total_time += time
-    min_rest = min(min_rest, limit - total_time)
-    # 총시간이 더 커지는경우는 제외하기 위해서 넣었다.
-    if limit < total_time:
-        print(-1)
-        exit()
-print(min_rest)
+input = __import__('sys').stdin.readline
+import sys
+n = int(input())
+l = []
+for i in range(n):
+    a,b = map(int,input().split())
+    l.append((b,a))
+l.sort(reverse=True)
+last = l[0][0]
+for i in l:
+    last = min(last,i[0])-i[1]
+    if last<0: print(-1); sys.exit()
+print(last)
