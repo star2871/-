5b7312c834e6b8a7abc77ecaf85b8c7460150827
Hee Soo Song <108647584+star2871@users.noreply.github.com>
2023-01-12 11:28:25 +0900

    [Gold V] Title: 시간 관리하기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/6068. 시간 관리하기/시간 관리하기.py
+++ 백준/Gold/6068. 시간 관리하기/시간 관리하기.py
@@ -1,13 +1,16 @@
-input = __import__('sys').stdin.readline
+input = __import__("sys").stdin.readline
 import sys
+
 n = int(input())
 l = []
 for i in range(n):
-    a,b = map(int,input().split())
-    l.append((b,a))
+    a, b = map(int, input().split())
+    l.append((b, a))
 l.sort(reverse=True)
 last = l[0][0]
 for i in l:
-    last = min(last,i[0])-i[1]
-    if last<0: print(-1); sys.exit()
+    last = min(last, i[0]) - i[1]
+    if last < 0:
+        print(-1)
+        sys.exit()
 print(last)
